448cda88e844a4e17546a8e8372ef428ba5abbb9
Skyler Richter <skylerrichter@gmail.com>
2018-02-25 11:35:04 -0800

    Update Thunk syntax.

--- src/actions/contract.js
+++ src/actions/contract.js
@@ -2,39 +2,33 @@ import TruffleContract from 'truffle-contract'
 import VotingContract from '../../build/contracts/Voting.json'
 import { CONTRACT_GET_CANDIDATES, CONTRACT_CAST_VOTE, CONTRACT_LOAD } from '../constants'
 
-export const castVote = (candidate) => {
-  return (dispatch, getState) => {
-    getState().contract.instance.voteForCandidate(candidate, {from: getState().web3.instance.eth.accounts[0]})
-      .then(() => {
-        dispatch({
-          type: CONTRACT_CAST_VOTE
-        })
+export const castVote = (candidate) => (dispatch, getState) => {
+  getState().contract.instance.voteForCandidate(candidate, {from: getState().web3.instance.eth.accounts[0]})
+    .then(() => {
+      dispatch({
+        type: CONTRACT_CAST_VOTE
       })
-  }  
+    })
 }
 
-export const getCandidates = () => {
-  return (dispatch, getState) => {
-    getState().contract.instance.getCandidateList.call()
-      .then((candidates) => {
-        dispatch({
-          type: CONTRACT_GET_CANDIDATES,
-          candidates
-        })
+export const getCandidates = () => (dispatch, getState) => {
+  getState().contract.instance.getCandidateList.call()
+    .then((candidates) => {
+      dispatch({
+        type: CONTRACT_GET_CANDIDATES,
+        candidates
       })
-  }
+    })
 }
 
-export const loadContract = () => {
-  return (dispatch, getState) => {
-    const voting = TruffleContract(VotingContract)
-    voting.setProvider(getState().web3.instance.currentProvider)
-    return voting.deployed()
-      .then((instance) => {
-        dispatch({
-          type: CONTRACT_LOAD,
-          instance
-        })
+export const loadContract = () => (dispatch, getState) => {
+  const voting = TruffleContract(VotingContract)
+  voting.setProvider(getState().web3.instance.currentProvider)
+  return voting.deployed()
+    .then((instance) => {
+      dispatch({
+        type: CONTRACT_LOAD,
+        instance
       })
-  }
+    })
 }
--- src/actions/web3.js
+++ src/actions/web3.js
@@ -1,34 +1,32 @@
 import Web3 from 'web3'
 import { WEB3_LOAD } from '../constants'
 
-export const fetchWeb3 = () => {
-  return (dispatch) => {
-    return new Promise(function(resolve, reject) {
-      window.addEventListener('load', function() {
-        var web3 = window.web3
+export const fetchWeb3 = () => (dispatch) => {
+  return new Promise((resolve, reject) => {
+    window.addEventListener('load', function() {
+      var web3 = window.web3
 
-        if (typeof web3 !== 'undefined') {
-          web3 = new Web3(web3.currentProvider)
-          dispatch({
-            type: WEB3_LOAD,
-            instance: web3
-          })
-          
-          resolve()
-        } else {
+      if (typeof web3 !== 'undefined') {
+        web3 = new Web3(web3.currentProvider)
+        dispatch({
+          type: WEB3_LOAD,
+          instance: web3
+        })
+        
+        resolve()
+      } else {
 
-          var provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545')
+        var provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545')
 
-          web3 = new Web3(provider)
+        web3 = new Web3(provider)
 
-          dispatch({
-            type: WEB3_LOAD,
-            instance: web3
-          })
+        dispatch({
+          type: WEB3_LOAD,
+          instance: web3
+        })
 
-          resolve()
-        }
-      })
+        resolve()
+      }
     })
-  }
+  })
 }
