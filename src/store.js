23750a04e413861d6b02c1358d346d47d80764ee
Skyler Richter <skylerrichter@gmail.com>
2018-03-02 17:05:58 -0800

    Normalize import.

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import ReduxThunk from 'redux-thunk'
-import rootReducer from './reducers/index'
+import reducers from './reducers'
 
 export default createStore(
-  rootReducer,
+  reducers,
   applyMiddleware(
     ReduxThunk
   )
