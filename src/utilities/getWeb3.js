8e02a03cf6fd51fa864c4e58c861abfaec06388b
Skyler Richter <skylerrichter@gmail.com>
2018-03-07 23:08:15 -0800

    Update constant name.

--- src/utilities/getWeb3.js
+++ src/utilities/getWeb3.js
@@ -1,10 +1,10 @@
 import _ from 'lodash'
 import Web3 from 'web3'
-import { LOCAL_NODE } from '../constants'
+import { DEFAULT_NODE_ADDRESS } from '../constants'
 
 const resolver = new Promise((resolve, reject) => {
   window.addEventListener('load', () => {
-    const web3 = new Web3(_.get(window.web3, 'currentProvider', new Web3.providers.HttpProvider(LOCAL_NODE)))
+    const web3 = new Web3(_.get(window.web3, 'currentProvider', new Web3.providers.HttpProvider(DEFAULT_NODE_ADDRESS)))
     resolve({
       web3
     })
